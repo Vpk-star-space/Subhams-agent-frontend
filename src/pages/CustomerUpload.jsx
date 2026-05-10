e9b1e22ad35cac0a94cf4878b2dbd6659675297a
Vnkata Pavan Kumar <pavanvenkat63@gmail.com>
2026-05-10 23:27:40 +0530

    fix bug

--- src/pages/CustomerUpload.jsx
+++ src/pages/CustomerUpload.jsx
@@ -806,15 +806,6 @@ useEffect(() => {
             <button onClick={clearHistory} style={{background: 'transparent', border: 'none', color: 'rgba(255,255,255,0.8)', fontSize: '12px', cursor: 'pointer', textDecoration: 'underline'}}>Clear All</button>
           </div>
 
-          <footer style={styles.footer}>
-                <p style={styles.footerText}>System Engineered by <strong>Venkata Pavan Kumar</strong></p>
-                <p style={styles.footerContact}>
-                    Contact: <a href="mailto:venkatapavankumar36@gmail.com" style={styles.footerLink}>venkatapavankumar36@gmail.com</a>
-                </p>
-                <p style={styles.footerContact}>
-                    Check out our other app: <a href="https://subhams-vpk.vercel.app/" target="_blank" rel="noopener noreferrer" style={styles.footerLink}>Subhams </a>
-                </p>
-            </footer>
           
           <div style={{ padding: '15px' }}>
             {activeOrders.map((order) => {
