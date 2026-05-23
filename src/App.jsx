0b1657fad68bf25960683f63eb3f76de769ebd9a
Venkata Pavan Kumar Amarthaluri <pavanvenkat63@gmail.com>
2026-05-24 01:14:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import LandingPage from './pages/LandingPage';
 import XeroxChatbot from './components/XeroxChatbot'; 
 
 // 🛑 THE MASTER SWITCH: Change to 'true' to lock down the app for updates!
-const IS_MAINTENANCE_MODE = false; 
+const IS_MAINTENANCE_MODE = true ; 
 const TARGET_LAUNCH_DATE = new Date("2026-05-26T10:00:00"); 
 
 // 🛡️ THE PROFESSIONAL BOUNCER (Only protects Shop Owners)
